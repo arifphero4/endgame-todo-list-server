98cd2da898ae3553c80eea31c4879593f7799754
arifphero <ahammadarif232@gmail.com>
2022-07-01 12:13:03 +0600

    user created

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wryro.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`to do web app listening on port ${port}`);
 });
